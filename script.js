cd854f19a6c82554ab0be92377dc9e744a7c913a
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-18 23:40:34 -0400

    Add code to improve the user experience with the email input element

--- script.js
+++ script.js
@@ -10,31 +10,36 @@ const dismiss = document.getElementById("dismiss");
 emailInput.oninvalid = e => {
   e.preventDefault();
   const tomato = getComputedStyle(document.documentElement).getPropertyValue("--tomato");
+  const lightTomato = getComputedStyle(document.documentElement).getPropertyValue("--light-tomato");
   emailInput.style.borderColor = tomato;
   emailInput.style.WebkitTextFillColor = tomato;
   emailInput.style.MozTextFillColor = tomato;
+  emailInput.style.backgroundColor = lightTomato;
   errorMessage.style.display = "block";
 };
 
 emailInput.addEventListener("focus", e => {
   const grey = getComputedStyle(document.documentElement).getPropertyValue("--grey");
   emailInput.style.borderColor = grey;
-  emailInput.style.WebkitTextFillColor = grey;
-  emailInput.style.MozTextFillColor = grey;
 });
 
 form.addEventListener("submit", e => {
   e.preventDefault();
+  const charcoalGrey = getComputedStyle(document.documentElement).getPropertyValue("--charcoal-grey");
+  const white = getComputedStyle(document.documentElement).getPropertyValue("--white");
   const formData = emailInput.value;
   userEmail.innerHTML = formData;
 
   signUpForm.style.display = "none";
-  successMessage.style.display = "block";
+  successMessage.style.display = "flex";
   errorMessage.style.display = "none";
   emailInput.value = "";
+  emailInput.style.WebkitTextFillColor = charcoalGrey;
+  emailInput.style.MozTextFillColor = charcoalGrey;
+  emailInput.style.backgroundColor = white;
 });
 
 dismiss.addEventListener("click", e => {
   successMessage.style.display = "none";
-  signUpForm.style.display = "block";
+  signUpForm.style.display = "flex";
 });
